247df4cac7e3ddb52f7057ba29fca9de2e8d2040
hetrank <hetrank@gmail.com>
2026-06-11 11:03:04 +0530

    Done with the structure setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Het Rank",
+    author_email="hetrank08@gmail.com",
+    packages=find_packages()
+)
